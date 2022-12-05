9c4afefff852520acfad3d1d62c522f13b89461f
micserv010821 <dotnet290320@gmail.com>
2022-12-05 21:15:08 +0200

    upload

--- promise_1/node1/node_await2.js
+++ promise_1/node1/node_await2.js
@@ -1,16 +1,30 @@
 
 function sleep(interval) {
-    
+    const sleep_promise = new Promise((resolve, reject) => {
+        setTimeout(
+            () => {  
+                    resolve()
+            },
+            interval);
+    })
+    return sleep_promise
 }
 
-const sleep_1000 = new Promise((resolve, reject) => {
-    setTimeout(
-        () => {  
-                resolve()
-        },
-        1000);
-})
-
+async function go_sleep() {
+    await new Promise((resolve, reject) => {
+        setTimeout(
+            () => {  
+                    resolve()
+            },
+            100);
+    })
+    console.log('finish sleep 100');
+    await sleep(200)
+    console.log('finish sleep 200');
+    await sleep(300)
+    console.log('finish sleep 300');
+}
+go_sleep()
 //await sleep 100
 //await sleep 200
 //await sleep 300
--- promise_1/node1/node_await3.js
+++ promise_1/node1/node_await3.js
@@ -1,17 +1,40 @@
 
+const axiosRequest = require('axios')
 
-axiosRequest.get(`https://www.boredapi.com/api/activity`)
-    .then(response => {
+async function run_axios() {
+    try {
+        const response = await axiosRequest.get(`https://www.boredapi.com/api/activity`);
         console.log(`You could : ${response.data.activity}`);
-        return axiosRequest.get(`https://www.boredapi.com/api/activity`)
-    })
-    .then(response => {
-        console.log(`You could : ${response.data.activity}`);
-        //return axiosRequest.get(`https://www.boredapi.com/api/activity`)
-    })
-    .catch(error => {
+        const response2 = await axiosRequest.get(`https://www.boredapi.com/api/activity`);
+        console.log(`You could : ${response2.data.activity}`);
+    }
+    catch (error) {
         console.log(`ERROR: ${error}`)
+    }
+    finished = true
+}
+
+async function run_axios_async() {
+    return new Promise(async (success, failure) => {
+        try {
+            const response = await axiosRequest.get(`https://www.boredapi.com/api/activity`);
+            console.log(`You could : ${response.data.activity}`);
+            const response2 = await axiosRequest.get(`https://www.boredapi.com/api/activity`);
+            console.log(`You could : ${response2.data.activity}`);
+        }
+        catch (error) {
+            console.log(`ERROR: ${error}`)
+        }
+        success()
     })
+}
+
+async function main() {
+    run_axios()
+    await run_axios_async()
+    await console.log('??????????');
+}
+main()
 
 //await call activity_api
 //await call activity_api    
