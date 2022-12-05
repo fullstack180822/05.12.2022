f8b642c3c66fd04ba54ccd44c68d21e5806c0dd9
micserv010821 <dotnet290320@gmail.com>
2022-12-05 19:09:54 +0200

    Update index1.js

--- promise_1/node1/index1.js
+++ promise_1/node1/index1.js
@@ -13,11 +13,31 @@ axiosRequest.get(`https://www.boredapi.com/api/activity`)
                             console.log(`ERROR: ${error}`)})
                          .then(response => {
                             console.log(`You could : ${response.data.activity}`);
-                            console.log('1111111111');
                             //return axiosRequest.get(`https://www.boredapi.com/api/activity`)
                         })
 
 // https://jsonplaceholder.typicode.com/todos/1
 // https://jsonplaceholder.typicode.com/albums/1
                       
+// json encoding
+const json_headers = {
+    headers: {
+        'Accept-Encoding': 'application/json',
+    }
+}
+axiosRequest.default.get(`https://jsonplaceholder.typicode.com/todos/1`, json_headers)
+    .then(response => {
+        console.log(`Todos: ${JSON.stringify(response.data)}`);
+        return axiosRequest.get(`https://jsonplaceholder.typicode.com/albums/1`, json_headers)
+    })
+    .catch(error => {
+        console.log(`ERROR: ${error}`)
+    })
+    .then(response => {
+        console.log(`Albums : ${JSON.stringify(response.data)}`);
+        //return axiosRequest.get(`https://www.boredapi.com/api/activity`)
+    })
+
+
+
 
